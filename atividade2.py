6bd8020243c150f311cfad0a514ce8b9d792a0bf
Faelavgtt <rafavergottii@gmail.com>
2024-10-10 11:00:56 -0400

    att2

---
+++ atividade2.py
@@ -0,0 +1,10 @@
+num = [1,2,3,4,5,6,7,8,9,10]
+
+def tabuada():
+    ip = int(input("Adicione um número inteiro:"))
+    for i in num:
+        resultado= ip * i
+        print(resultado)
+tabuada()
+
+
