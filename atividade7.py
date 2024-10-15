00e257bf7bc4e38546b6e37015d7879cf96e2286
Faelavgtt <rafavergottii@gmail.com>
2024-10-15 08:28:21 -0400

    finalizado

--- atividade7.py
+++ atividade7.py
@@ -1,1 +1,21 @@
-#  Faça um programa que converta uma temperatura de Celsius para Fahrenheit e vice-versa. O usuário 
+#  Faça um programa que converta uma temperatura de Celsius para Fahrenheit e vice-versa. O usuário deverá escolher a conversão desejada. 
+
+
+# f=0.5555555555555556
+# c=1.8
+
+while (True):
+    selecao = input("Conversão de °C ou °F:")
+    if selecao =="c" or selecao =="C" or selecao == "celsius":
+        temperatura = float(input("Adicione para conversão:"))
+        c=0.5555555555555556 * temperatura - 32
+        print(f"a temperatura é {c}")
+    elif selecao =="f" or selecao == "F" or selecao == "Fahrenheit":
+        temperatura = float(input("Adicione para conversão:"))
+        f=1.8 * temperatura + 32
+        print(f"a temperatura é {f}")
+    elif selecao == "sair" or selecao == "quit":
+        quit()
+
+    else:
+        print("Coloque F ou C(ou escreva corretamente os nomes)")
--- atividade8.py
+++ atividade8.py
@@ -1,1 +1,18 @@
 # Crie um algoritmo que receba 5 números e exiba o maior e o menor número informados.
+
+# def maior_menor():
+numeros = []
+
+for i in range(5):
+    numero = int(input("Adicione o número: "))
+    numeros.append(numero)
+menor = maior = numeros[0]
+
+for numero in numeros:
+    if numero < menor:
+        menor = numero
+    if numero > maior:
+        maior = numero
+
+print("Menor número:", menor)
+print("Maior número:", maior)
